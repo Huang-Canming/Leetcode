bfc94a8b75a10417783d4952630534451c4797f4
root <root@LAPTOP-C3TTEKVC.localdomain>
2025-02-12 21:45:55 +0800

    leetcode 207

--- leetcode_207_canFinish/canFinish.cpp
+++ leetcode_207_canFinish/canFinish.cpp
@@ -1,87 +1,67 @@
+#include <iostream>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
 class Solution {
-private:
-    vector<vector<int>> edges;
-    vector<int> indeg;
 public:
-    #if 0
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int course = 0;
-        queue<int> queCourse;
-        vector<int> res;
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+        queue<int> goFinish;
+        int finishNum = 0;
+        int prevCourse = 0;
+        int nextCourse = 0;
 
-        /* 转化为邻接矩阵 */
-        edges.resize(numCourses);
-        indeg.resize(numCourses);
-        for (auto vecPreReq : prerequisites) {
-            edges[vecPreReq[0]].push_back(vecPreReq[1]);
-            indeg[vecPreReq[0]]++;
+        // 构建邻接矩阵和入度表
+        for (int i = 0; i < prerequisites.size(); i++) {
+            // 边的方向是从 prerequisites[i][1] 指向 prerequisites[i][0]
+            prevCourse = prerequisites[i][1];
+            nextCourse = prerequisites[i][0];
+            graph[prevCourse].push_back(nextCourse);
+            indegree[nextCourse]++;
         }
 
+        // 所有入度为 0 的节点入队列
         for (int i = 0; i < numCourses; i++) {
-            if (0 == indeg[i]) {
-                queCourse.push(i);
-                indeg[i] = -1;
+            if (0 == indegree[i]) {
+                goFinish.push(i);
             }
         }
 
-        while (!queCourse.empty()) {
-            course = queCourse.front();
-            res.push_back(course);
-            for (int i = )
-        }
-    }
-    #endif
-
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        edges.resize(numCourses);
-        indeg.resize(numCourses);
-        for (const auto& info: prerequisites) {
-            edges[info[1]].push_back(info[0]);
-            ++indeg[info[0]];
-        }
-
-        queue<int> q;
-        for (int i = 0; i < numCourses; ++i) {
-            if (indeg[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        int visited = 0;
-        while (!q.empty()) {
-            ++visited;
-            int u = q.front();
-            q.pop();
-            for (int v: edges[u]) {
-                --indeg[v];
-                if (indeg[v] == 0) {
-                    q.push(v);
+        // 访问队列中的节点（出队）
+        while (0 != goFinish.size()) {
+            prevCourse = goFinish.front();
+            goFinish.pop();
+            finishNum++;
+            // 若有所指向的节点，则所指节点入度减 1
+            for (int i = 0; i < graph[prevCourse].size(); i++) {
+                nextCourse = graph[prevCourse][i];
+                indegree[nextCourse]--;
+                // 若所指节点入度减 1 后变为 0，同样加入队列
+                if (0 == indegree[nextCourse]) {
+                    goFinish.push(nextCourse);
                 }
             }
         }
 
-        return visited == numCourses;
+        if (finishNum == numCourses) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
 
 int main() {
     Solution solution;
     vector<vector<int>> prerequisites;
-    prerequisites.push_back({0, 2});
-    prerequisites.push_back({0, 4});
-    prerequisites.push_back({0, 5});
     prerequisites.push_back({1, 0});
-    prerequisites.push_back({1, 2});
-    prerequisites.push_back({3, 5});
-    prerequisites.push_back({5, 4});
-    prerequisites.push_back({5, 6});
+    cout << solution.canFinish(2, prerequisites) << endl;
 
-    solution.canFinish(7, prerequisites);
+    prerequisites.push_back({0, 1});
+    cout << solution.canFinish(2, prerequisites) << endl;
 
     return 0;
 }
