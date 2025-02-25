f390b8d5070dd1a63feed3034825ab37d63e8368
root <root@LAPTOP-C3TTEKVC.localdomain>
2025-02-26 00:31:40 +0800

    leetcode 76

--- leetcode_76_minWindow/minWindow.cpp
+++ leetcode_76_minWindow/minWindow.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <limits.h>
 
 using namespace std;
 
@@ -38,9 +39,9 @@ public:
             }
         }
 
+        // s 不满足
         if (fullCharCnt < totalCharCnt)
         {
-            // s 不满足
             return "";
         }
         
@@ -87,7 +88,45 @@ public:
         }
 
         return s.substr(miniWow.first, miniWow.second - miniWow.first + 1);
-    } 
+    }
+
+    unordered_map <char, int> ori, cnt;
+
+    bool check() {
+        for (const auto &p: ori) {
+            if (cnt[p.first] < p.second) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    string minWindow_offical(string s, string t) {
+        for (const auto &c: t) {
+            ++ori[c];
+        }
+
+        int l = 0, r = -1;
+        int len = INT_MAX, ansL = -1, ansR = -1;
+
+        while (r < int(s.size())) {
+            if (ori.find(s[++r]) != ori.end()) {
+                ++cnt[s[r]];
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                }
+                if (ori.find(s[l]) != ori.end()) {
+                    --cnt[s[l]];
+                }
+                ++l;
+            }
+        }
+
+        return ansL == -1 ? string() : s.substr(ansL, len);
+    }
 };
 
 int main () {
